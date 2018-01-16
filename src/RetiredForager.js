b6a42ddb10828f49e3ea656669958704735a3357
Eduardo091 <urrutia.e14@gmail.com>
2018-01-15 19:37:13 -0600

    finished bees

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,14 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = "gamble";
+  this.canFly = false;
+  this.color = "grey";
+  RetiredForagerBee.prototype.forage = function("I am too old, let me play cards instead")
 };
-
+ RetiredForagerBee.prototype.gamble = function(treasure){
+   this.treasureChest.push(treasure);
+ }
+};
+ RetiredForagerBee.prototype = Object.create(Bee.protoype);
+ RetiredForagerBee.prototype.constructor = RetiredForagerBee;
